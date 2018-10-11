b9eaa51e660d30dd0d6a95736974cf4564c532df
Альмир <43670110+Gweejay@users.noreply.github.com>
2018-10-11 14:37:02 +0500

    Commit 12x
    осталось настроить корректное отображение файлов !(.txt)

--- Server/src/HttpServer.java
+++ Server/src/HttpServer.java
@@ -31,10 +31,10 @@ public class HttpServer {
         private String http = "HTTP/1.1";
         private String methodtype = null;
         private String resourse = null;
-        private String parameters = null;
         private int code = 500;
         private String message = "Internal Server Error";
         private String response = "";
+        private String Allow = "GET, POST, UPDATE, DELETE";
 
         private void transfer (int code, String message) {
             this.transfer (code,message,"");
@@ -48,6 +48,13 @@ public class HttpServer {
             if (response != "") {
                 this.response = response;
             }
+        }
+
+        private void transfer (int code, String message,String response, String allow) {
+            if (allow != "") {
+                this.Allow = allow;
+            }
+            this.transfer (code,message,response);
         }
 
         private SocketProcessor(Socket s) throws Throwable {
@@ -73,7 +80,7 @@ public class HttpServer {
         }
 
         private void writeResponse(String str) throws Throwable {
-            /* История запросов */
+
             try (FileWriter writer = new FileWriter ("General/History.txt", true)) {
                 DateFormat dateFormat = new SimpleDateFormat (" dd/MM/yy HH:mm");
                 Date date = new Date();
@@ -86,13 +93,9 @@ public class HttpServer {
             String response;
             response = http + " " + code + " " + message + "\r\n" +
                     "Content-Type: text/html; charset=utf-8\r\n" +
-//                    "Accept-Charset: utf-8\r\n" +
-//                    "Accept-Language: ru\r\n" +
-//                    "Content-Language: ru\r\n" +
                     "Content-Length: " + str.length () + "\r\n" +
-                    "Allow: GET, POST, UPDATE, DELETE \r\n" +
+                    "Allow: " + Allow + "\r\n" +
                     "Connection: close\r\n\r\n";
-
 
             String result = response + str;
             os.write(result.getBytes());
@@ -127,7 +130,7 @@ public class HttpServer {
         }
 
         private void delete ()  {
-            if (resourse != "") {
+            if (resourse.substring (0,2).equalsIgnoreCase ("id")) {
                 File file = new File (resourse);
                 if (file.delete ()) {
                     transfer (202, "Accepted", "Файл успешно удален");
@@ -135,23 +138,22 @@ public class HttpServer {
                     transfer (404, "Not Found", "Not Found");
                 }
             } else {
-                transfer (400, "Bad Request", "Bad Request");
+                transfer (403, "Forbidden", "Error: Аccess denied");
             }
         }
 
         private int newID (int id) {
-
             try (BufferedReader uploader = new BufferedReader (new InputStreamReader (new FileInputStream ("General/Base.txt"), StandardCharsets.UTF_8))) {
                 String str = uploader.readLine ();
                 id += Integer.parseInt (str);
                 uploader.close ();
             } catch (IOException e) { }
-
             try (FileWriter uploader = new FileWriter ("General/Base.txt", false)) {
                 uploader.append (id + "");
                 uploader.flush ();
                 uploader.close ();
             } catch (IOException e) { }
+
             return id;
         }
 
@@ -160,9 +162,13 @@ public class HttpServer {
             int x = s.indexOf (index);
             int y = index.length ();
 
-            s = s.substring (x + y, s.length());
-            s = s.substring (0, s.indexOf ("\""));
-            s = s.substring (s.indexOf ("."), s.length ());
+            if (x>0) {
+                s = s.substring (x + y, s.length ());
+                s = s.substring (0, s.indexOf ("\""));
+                s = s.substring (s.indexOf ("."), s.length ());
+            } else {
+                s=".txt";
+            }
 
             return s;
         }
@@ -207,10 +213,9 @@ public class HttpServer {
                         case 2:
                             resourse = str.substring (1, str.length ());
                             break;
-                        case 3:
-//                            http = str; /* don't save */
+                        default:
+                            { /*do nothing*/ }
                             break;
-                        default: { /*do nothing*/ }
                     }
                 }
                 if ( y < 3 ) {
@@ -225,14 +230,8 @@ public class HttpServer {
         private String readRequest() throws Throwable {
 
             String Operator = "";
-
-            String[] lines = new String[120];
-            DateFormat dateFormat = new SimpleDateFormat (" dd/MM/yy HH:mm");
-            Date date = new Date();
-
             int streamSize = 8192;
             byte[] buffer = new byte[streamSize];
-            FileWriter writer = new FileWriter ("General/Operator.txt", false);
             boolean once = false;
 
             while (true){
@@ -242,38 +241,22 @@ public class HttpServer {
                     /* Чтение потока отправленного через браузер */
                     String FL = new String (buffer).substring (0, new String (buffer).indexOf ("\r\n"));
                     checkFL (FL);
-
-                    if (true)
+                    while (readBytesCount == streamSize)
                     {
-                        while (readBytesCount == streamSize)
-                        {
-                            Operator = Operator + (new String (buffer).trim ());
-                            if ("POST".equalsIgnoreCase (methodtype)) {
-                                writer.write (new String (buffer).trim ());
-                            }
-                            readBytesCount = is.read (buffer);
-                        }
                         Operator = Operator + (new String (buffer).trim ());
-                        if (methodtype.equalsIgnoreCase ("POST")) {
-                            writer.write (new String (buffer).trim ());
-                            System.out.println ("Записал поток на txt");
-                        }
-                        else {
-                            System.out.println (methodtype + " информация не записана");
-                        }
-                        writer.flush ();
-
+                        readBytesCount = is.read (buffer);
                     }
+                    Operator = Operator + (new String (buffer).trim ());
                     break;
                 }
                 else { /* Чтение потока отправленного через telnet (посимвольно) */
                     if (readBytesCount == 1 || readBytesCount > 3 ) { /*Символ или строка(для корректной работы требуется не менее 4 байт информации)*/
                         System.out.println (new String (buffer).trim ());
                         if ( (new String (buffer).trim ().length ()) == 0 ) {
-                            writer.write (" "); /*Запись пробела*/
+                            Operator += " ";
                         }
                         else {
-                            writer.write (new String (buffer).trim ());
+                            Operator += (new String (buffer).trim ());
                         }
                     }
                     if (readBytesCount == 2) { /*Enter*/
@@ -281,7 +264,8 @@ public class HttpServer {
                             break;
                         }
                         System.out.println ("Enter");
-                        writer.append("\r\n").write (new String (buffer).trim ());
+                        Operator += "\r\n";
+                        checkFL (Operator);
                         once = true;
                     }
                     else { once = false; }
@@ -290,97 +274,45 @@ public class HttpServer {
                         transfer ( 400, "Bad request", "Error of read stream");
                         break;
                     }
-                    writer.flush ();
                 }
             }
-            writer.close ();
-            /* Second checking method type (for TELNET)*/
-            if (code == 500) {
-                try (BufferedReader reader = new BufferedReader (new InputStreamReader (new FileInputStream ("General/Operator.txt"), StandardCharsets.UTF_8))) {
-                    if (methodtype == null) {
-                        String FL = reader.readLine ().trim ();
-                        if (true) {
-                            checkFL (FL);
-                        }
+            switch (methodtype) {
+                case "GET": {
+                    if (access ()) get ();
+                } break;
+
+                case "UPDATE": {
+                    update (" ");
+                } break;
+
+                case "DELETE": {
+                    if (access ()) delete ();
+                } break;
+
+                case "POST": {
+                    Operator = Operator.substring ((Operator.indexOf ("\r\n\r\n")+4), Operator.length ());
+                    String address = "id" + newID(1) + format(Operator);
+                    if (Operator.indexOf ("------WebKitForm")>=0) {
+                        Operator = Operator.substring ((Operator.indexOf ("\r\n\r\n") + 4), Operator.length ());
+                        Operator = Operator.substring (0, (Operator.indexOf ("------WebKitForm")));
                     }
-                } finally {
+                    FileWriter writer = new FileWriter ( address, false);
+                    writer.write(Operator);
+                    transfer (201, "Created", "<p>Successfully created: your file is on "
+                            + "<a href =\""
+                            + "http://localhost:8080/" + address
+                            + "\">Ссылка</a></p>");
+                    writer.flush ();
+                    writer.close();
+                } break;
 
-                    switch (methodtype) {
+                default: {
 
-                        case "GET": {
-                            if (access ()) get ();
-                        }
-                        break;
-
-                        case "UPDATE": {
-                            update (dateFormat.format (date));
-                        }
-                        break;
-
-                        case "DELETE": {
-                            if (access ()) delete ();
-                        }
-                        break;
-
-                        case "POST": {
-
-                            Operator = Operator.substring ((Operator.indexOf ("\r\n\r\n")+4), Operator.length ());
-
-                            String address = "id" + newID(1) + format(Operator);
-
-                            Operator = Operator.substring ((Operator.indexOf ("\r\n\r\n")+4), Operator.length ());
-
-                            Operator = Operator.substring (0,(Operator.indexOf ("------WebKitForm")));
-
-                            writer = new FileWriter ( address, false);
-
-
-
-                            /* Ввод данных*/
-                            writer.write(Operator);
-                            /* Вывод данных*/
-                            transfer (201, "Created", "<p>Successfully created: your file is on "
-                                    + "<a href =\""
-                                    + "http://localhost:8080/"
-                                    + address
-                                    + "\">Ссылка</a></p>"
-
-                            );
-
-/**
- *Отбор основных заголовов запроса
- *Должен применяться для всех методов
- *
- * */
-//                            String[] mainHeaders, mainHeadersValue;
-//                            mainHeaders = new String[30];
-//                            mainHeadersValue = new String[30];
-//
-//                            for (int i = 1, x, y; i <= h; i++) {
-//                                if (lines[i].indexOf (":") > 0) {
-//                                    x = lines[i].indexOf (":");
-//                                    y = lines[i].length ();
-//                                    mainHeaders[i] = lines[i].substring (0, x);
-//                                    mainHeadersValue[i] = lines[i].substring (x + 2, y);
-//                                }
-//                            }
-                            writer.append ("\r\n");
-                            writer.flush ();
-                            writer.close();
-                        }
-                        break;
-
-                        default: {
-
-                            if (code == 500) {
-//                            transfer (405,"Method Not Allowed", "Method Not Allowed");
-                                transfer (501, "Not Implemented", "Not Implemented");
-                            }
-                            methodtype = "Undefined";
-                        }
-                        break;
+                    if (code == 100) {
+                        transfer (501, "Not Implemented", "Not Implemented");
                     }
-                }
+                    methodtype = "Undefined";
+                } break;
             }
             return methodtype;
         }
